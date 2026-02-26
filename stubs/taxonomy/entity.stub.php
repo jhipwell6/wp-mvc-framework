958df8f586a9788efff2e0319b2bd2c1c12f218c
Jason <13647112+jhipwell6@users.noreply.github.com>
2026-02-26 07:10:19 -0500

    Rename taxonomy stubs to canonical paths

---
+++ stubs/taxonomy/entity.stub.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace {{app_namespace}}\Domain\Taxonomies\{{taxonomy.class}}\Generated;
+
+class {{taxonomy.class}}Base
+{
+    public function __construct(
+        public ?int $id = null,
+        public string $name = '',
+        public string $slug = '',
+        public string $description = '',
+        public int $parent = 0,
+{{taxonomy.fields}}
+    ) {
+    }
+}
